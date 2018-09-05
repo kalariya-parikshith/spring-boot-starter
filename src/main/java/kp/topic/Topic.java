2c3346012f3b489ef8b2b6b618ce4f236a3b2ced
kalariya-parikshith <k.parikshith93@gmail.com>
2018-09-05 12:44:31 +0530

    first commit

---
+++ src/main/java/kp/topic/Topic.java
@@ -0,0 +1,30 @@
+package kp.topic;
+
+public class Topic {
+	private String id;
+	private String name;
+	
+	public Topic() {
+		
+	}
+	
+	public Topic(String id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ src/main/java/kp/topic/TopicService.java
@@ -0,0 +1,43 @@
+package kp.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	private List<Topic> topics = new ArrayList<>( Arrays.asList(
+			new Topic("1","kp"),
+			new Topic("2","pk")
+			));
+	
+	public List<Topic> getAllTopics(){
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+	
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+	public void updateTopic(String id, Topic topic) {
+		for(int i=0; i<topics.size(); i++) {
+			Topic t = topics.get(i);
+			if(t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+		
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
+	}
+}
