d6a11357da64349bb657ccad359fad4ac0795fef
kalariya-parikshith <k.parikshith93@gmail.com>
2018-09-05 14:40:11 +0530

    hello world message

--- src/main/java/kp/CourseApiApp.java
+++ src/main/java/kp/CourseApiApp.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CourseApiApp {
+public class
+CourseApiApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApiApp.class, args);
-
+		System.out.println("Hello world");
 	}
 
 }
